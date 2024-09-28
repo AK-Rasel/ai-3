e1f29b68ca674e4b7c271c1b0f3c9a9797a74447
AK-Rasel <raselak101010@gmail.com>
2024-09-29 00:53:43 +0600

    fille uploded dainamic

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -1,40 +1,9 @@
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  PutObjectCommand,
+  ListObjectsV2Command,
+} from "@aws-sdk/client-s3";
 import OpenAI from "openai";
-
-const openai = new OpenAI({
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
-});
-
-const myKeyWord = {
-  bangladesh: "bangladesh is small country",
-  dhaka: "dhaka is big city",
-};
-const myKeyWordFindKeys = Object.keys(myKeyWord);
-export async function generateAIVoice(file) {
-  // eslint-disable-next-line no-useless-catch
-  try {
-    const transcription = await openai.audio.transcriptions.create({
-      file,
-      model: "whisper-1",
-    });
-    const text = [];
-    myKeyWordFindKeys.forEach((item) => {
-      if (transcription.text.toLowerCase().includes(item.toLowerCase())) {
-        text.push(myKeyWord[item]);
-      }
-    });
-
-    // const mp3 = await openai.audio.speech.create({
-    //   model: "tts-1",
-    //   voice: "alloy",
-    //   input: transcription.text,
-    // });
-    // return mp3;
-  } catch (error) {
-    throw error;
-  }
-}
 
 const s3 = new S3Client({
   region: "auto",
@@ -45,18 +14,81 @@ const s3 = new S3Client({
   },
 });
 
-export async function uploadAIVoice(file) {
-  const blob = await file.blob();
-  const audio = new File([blob], "audio.mp3", { type: blob.type });
+// key word------------------------------------------------------------------
+const myKeyWord = {
+  bangladesh: "bangladesh is small country",
+  dhaka: "dhaka is big city",
+  world: "hello world",
+};
+
+const myKeyWordFindKeys = Object.keys(myKeyWord);
+
+const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+export async function generateAIVoice(file) {
   // eslint-disable-next-line no-useless-catch
   try {
-    const command = new PutObjectCommand({
-      Bucket: import.meta.env.VITE_R2_BUCKET_NAME,
-      Key: Math.random().toString(36) + ".mp3",
-      Body: audio,
-      ContentType: "audio/mpeg",
+    const transcription = await openai.audio.transcriptions.create({
+      file,
+      model: "whisper-1",
+      language: "en",
     });
-    await s3.send(command);
+    console.log(transcription.text);
+    const text = [];
+
+    myKeyWordFindKeys.forEach((item) => {
+      if (transcription.text.toLowerCase().includes(item.toLowerCase())) {
+        text.push([item, myKeyWord[item]]);
+      }
+    });
+
+    console.log(text[0]);
+    const mp3 = await openai.audio.speech.create({
+      model: "tts-1",
+      voice: "alloy",
+      input: text[0][1],
+      language: "en",
+    });
+
+    return [text[0][0], mp3];
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function uploadAIVoice(file) {
+  const [a, b] = file;
+  const blob = await b.blob();
+
+  const audio = new File([blob], "audio.mp3", { type: blob.type });
+  const params = {
+    Bucket: import.meta.env.VITE_R2_BUCKET_NAME,
+  };
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const data = await s3.send(new ListObjectsV2Command(params));
+    const mp3Files = data.Contents.filter((file) => file.Key.endsWith(".mp3"));
+    for (const file of mp3Files) {
+      const keysSplit = file.Key.split(".")[0];
+
+      if (!myKeyWordFindKeys.includes(keysSplit)) {
+        console.log(myKeyWordFindKeys);
+        const command = new PutObjectCommand({
+          Bucket: import.meta.env.VITE_R2_BUCKET_NAME,
+          Key: a + ".mp3", // To Do letter ----------------------------------------------------------------------
+          Body: audio,
+          ContentType: "audio/mpeg",
+        });
+        // Send the upload command
+        await s3.send(command);
+        console.log("File uploaded successfully!");
+      } else {
+        console.log("File Not uploaded");
+      }
+    }
   } catch (error) {
     throw error;
   }
